d19083a3082b75f6a7ae5983014cb107715c4381
taopupu <18292835521@163.com>
2017-09-06 11:39:23 +0800

    Initial commit

---
+++ app/src/main/java/com/taopu/videotest/TestActivity.java
@@ -0,0 +1,19 @@
+package com.taopu.videotest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.etiennelawlor.imagegallery.library.activities.ImageGalleryActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
